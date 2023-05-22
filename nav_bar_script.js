513bd5a4b528a4dc15e12112a107f1ac434cef88
Ravindu Weerasinghe <raviweeras@gmail.com>
2023-05-22 22:51:01 +1000

    nav bar change

--- nav_bar_script.js
+++ nav_bar_script.js
@@ -4,9 +4,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("topnav").style.padding = "1px 1px";
-    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("logo").style.fontSize = "3vw";
   } else {
     document.getElementById("topnav").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("logo").style.fontSize = "6vw";
   }
 }
