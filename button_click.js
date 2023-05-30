5e4d31b84623d35ced8e859d2a50aab67fd37262
Ravindu Weerasinghe <raviweeras@gmail.com>
2023-05-30 15:06:09 +1000

    button popup init

---
+++ button_click.js
@@ -0,0 +1,13 @@
+
+$ = function(id){
+    return document.getElementById(id);
+}
+
+var show = function(id){
+    $(id).style.display = 'block';
+}
+
+var hide = function(id){
+    $(id).style.display = 'none';
+}
+
